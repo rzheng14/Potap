ee84d5c7bf25d9bdbece0e4869080617a133e6a2
Richard Zheng <rzheng14@binghamton.edu>
2020-12-22 13:31:00 -0500

    Initial Commit

---
+++ Potap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Potap
+//
+//  Created by Richard Zheng on 12/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
